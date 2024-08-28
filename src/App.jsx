011d4b658363d08f4e3e018f94d67b26bb5cd451
varun <rvarungoud20@gmail.com>
2024-08-28 19:16:06 +0530

    created authorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
 const App = () => {
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <div className='w-screen h-screen pt-4 text-green-200 bg-black  '>
-      <h1 >Mega Project</h1>
-      <h1 >Mega Project</h1>
-      <h1 >Mega Project</h1>
-      <h1 >Mega Project</h1>
+      <h1 className='text-3xl'>Mega Project</h1>
+      
     </div>
-  )
+  ) 
 }
 
 export default App 
