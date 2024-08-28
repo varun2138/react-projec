74630342d16f2f3fa03703663642f395d1426671
varun <rvarungoud20@gmail.com>
2024-08-28 17:38:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen pt-4 text-green-200 bg-black  '>
+      <h1 >Mega Project</h1>
+    </div>
+  )
+}
+
+export default App 
