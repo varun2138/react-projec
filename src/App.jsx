8f98cc08200dadb444801b2a0b8f2687d9af3cd9
varun <rvarungoud20@gmail.com>
2024-08-28 17:42:43 +0530

    env added to gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div className='w-screen h-screen pt-4 text-green-200 bg-black  '>
       <h1 >Mega Project</h1>
+      <h1 >Mega Project</h1>
+      <h1 >Mega Project</h1>
+      <h1 >Mega Project</h1>
     </div>
   )
 }
